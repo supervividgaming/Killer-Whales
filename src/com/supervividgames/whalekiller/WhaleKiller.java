3916dede427838d1a9066eee4303b06c0d0b0d36
Chris Statham <supervividgaming@gmail.com>
2011-05-18 15:28:52 -0400

    Setting up the default Android package layout

---
+++ src/com/supervividgames/whalekiller/WhaleKiller.java
@@ -0,0 +1,15 @@
+package com.supervividgames.whalekiller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WhaleKiller extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
